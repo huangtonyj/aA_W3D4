586524d6fe3035de3da861da5cbefc335986634b
huangtonyj <huangtonyj91@gmail.com>
2018-09-06 16:41:04 -0700

    Create poll app models

--- polls/polls_app/app/models/question.rb
+++ polls/polls_app/app/models/question.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id            :bigint(8)        not null, primary key
+#  poll_id       :integer          not null
+#  question_text :text             not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Question < ApplicationRecord
+  
+  has_many :answer_choices,
+    primary_key: :id,
+    foreign_key: :question_id,
+    class_name: :AnswerChoice
+    
+  belongs_to :poll,
+    primary_key: :id,
+    foreign_key: :poll_id,
+    class_name: :Poll
+  
 end
--- polls/polls_app/app/models/response.rb
+++ polls/polls_app/app/models/response.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: responses
+#
+#  id               :bigint(8)        not null, primary key
+#  responder_id     :integer          not null
+#  answer_choice_id :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Response < ApplicationRecord
+  
+  belongs_to :answer_choice,
+    primary_key: :id,
+    foreign_key: :answer_choice_id,
+    class_name: :AnswerChoice
+    
+  belongs_to :respondent,
+    primary_key: :id,
+    foreign_key: :responder_id,
+    class_name: :User
+  
 end
